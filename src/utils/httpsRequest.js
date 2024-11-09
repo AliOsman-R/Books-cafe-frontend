183603d112f7b36d7bba1a3f591d5d6561bc479f
AliCoolriots <ali.osman@coolriots.io>
2024-11-10 02:10:30 +0300

    update httpRequest file

--- src/utils/httpsRequest.js
+++ src/utils/httpsRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const server = process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api/v1' : ''
+export const server = process.env.REACT_APP_NODE_ENV === 'dev' ? 'http://localhost:5000/api/v1' : 'https://books-cafe-backend.vercel.app/api/v1'
 
 
 export const httpRequest = axios.create({
